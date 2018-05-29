b5db8dc16d2836167fce3e364c183c93e639cd2a
uuuu <kolos181@gmail.com>
2018-05-29 16:48:17 +0300

    added two way binding and add User button

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
-import { User } from '../../models/User'; 
+import {User} from '../../models/User';
 
 @Component({
   selector: 'app-users',
@@ -8,59 +8,89 @@ import { User } from '../../models/User';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  user: User = {
+    firstName: '',
+    lastName: '',
+    age: null,
+    address: {
+      street: '',
+      city: '',
+      state: ''
+    }
+  }
   users: User[];
   showExtended: boolean = true;
   loaded: boolean = false;
-  enableAdd: boolean = true;
+  enableAdd: boolean = false;
+  showUserForm = false;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
-   
-      this.users = [
-        {
-          firstName: 'John',
-          lastName: 'Doe',
-          age: 70,
-          address: {
-            street: '50 Main st',
-            city: 'Boston',
-            state: 'MA'
-          },
-          isActive: true,
-          registered: new Date('01/02/2018 08:30:00')
-        },
-        {
-          firstName: 'Kevin',
-          lastName: 'Johnson',
-          age: 34,
-          address: {
-            street: '20 School st',
-            city: 'Lynn',
-            state: 'MA'
-          },
-          isActive: false,
-          registered: new Date('03/11/2017 06:20:00')
-        },
-        {
-          firstName: 'Karen',
-          lastName: 'Williams',
-          age: 26,
-          address: {
-            street: '55 Mill st',
-            city: 'Miami',
-            state: 'FL'
-          },
-          isActive: true,
-          registered: new Date('11/02/2016 10:30:00')
-        }
-      ];
 
-      this.loaded = true;
+    this.users = [
+      {
+        firstName: 'John',
+        lastName: 'Doe',
+        age: 70,
+        address: {
+          street: '50 Main st',
+          city: 'Boston',
+          state: 'MA'
+        },
+        isActive: true,
+        registered: new Date('01/02/2018 08:30:00'),
+        hide: true
+      },
+      {
+        firstName: 'Kevin',
+        lastName: 'Johnson',
+        age: 34,
+        address: {
+          street: '20 School st',
+          city: 'Lynn',
+          state: 'MA'
+        },
+        isActive: false,
+        registered: new Date('03/11/2017 06:20:00'),
+        hide: true
+      },
+      {
+        firstName: 'Karen',
+        lastName: 'Williams',
+        age: 26,
+        address: {
+          street: '55 Mill st',
+          city: 'Miami',
+          state: 'FL'
+        },
+        isActive: true,
+        registered: new Date('11/02/2016 10:30:00'),
+        hide: true
+      }
+    ];
+
+    this.loaded = true;
   }
 
-  addUser(user: User) {
-    this.users.push(user);
+  addUser() {
+    this.users.unshift(this.user);
+    this.user.isActive = true;
+    this.user.registered = new Date();
   }
-  
+
+  // toggleHide(user: User) {
+  //   user.hide = !user.hide;
+  // }
+
+  onSubmit(e) {
+    console.log(e.type);
+    e.preventDefault();
+  }
+
+  fireEvent(e) {
+    console.log(e.type);
+    console.log(e.target.value);
+  }
 }
